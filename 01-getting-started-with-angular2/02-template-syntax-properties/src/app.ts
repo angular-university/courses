5e6a32980d165ec2da66a2141a81e337c641e3db
Your Name <you@example.com>
2016-04-27 11:53:30 +0200

    Template Properties

--- 01-getting-started-with-angular2/02-template-syntax-properties/src/app.ts
+++ 01-getting-started-with-angular2/02-template-syntax-properties/src/app.ts
@@ -2,6 +2,7 @@
 import {Component} from "angular2/core";
 import {bootstrap} from "angular2/platform/browser";
 
+
 @Component({
     selector: 'app',
     template: `
@@ -18,6 +19,8 @@ export class App {
 
 
 }
+
+
 
 bootstrap(App);
 

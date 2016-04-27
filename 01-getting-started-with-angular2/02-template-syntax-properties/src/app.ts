39a21c7b55e76759c68fd60e790cda77507fe795
Your Name <you@example.com>
2016-04-27 10:39:30 +0200

    Template Properties

---
+++ 01-getting-started-with-angular2/02-template-syntax-properties/src/app.ts
@@ -0,0 +1,15 @@
+
+import {Component} from "angular2/core";
+import {bootstrap} from "angular2/platform/browser";
+
+@Component({
+    selector: 'app', 
+    template: `<input [style.color]="red">`
+})
+export class App {
+
+
+}
+
+bootstrap(App);
+

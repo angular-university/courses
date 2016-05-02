48eaec86164ec70cd9a7dfc78b566c62526b75e3
Your Name <you@example.com>
2016-05-02 09:40:20 +0200

    Lesson 3 - Template Syntax - Events and Introduction to Zones

---
+++ 01-getting-started-with-angular2/src/03-template-syntax-interpolation/lesson.ts
@@ -0,0 +1,22 @@
+
+import {Component} from "angular2/core";
+import {bootstrap} from "angular2/platform/browser";
+
+
+@Component({
+    selector: 'app',
+    template: `
+
+
+        `
+})
+export class App {
+
+
+
+}
+
+
+
+bootstrap(App);
+

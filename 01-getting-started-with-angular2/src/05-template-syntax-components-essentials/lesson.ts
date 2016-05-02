0f040d5378aaa988624bf8ae9dc85eca0efddad8
Your Name <you@example.com>
2016-05-02 11:32:49 +0200

    Lesson 5 - Components Essentials

--- 01-getting-started-with-angular2/src/05-template-syntax-components-essentials/lesson.ts
+++ 01-getting-started-with-angular2/src/05-template-syntax-components-essentials/lesson.ts
@@ -7,6 +7,16 @@ import {bootstrap} from "angular2/platform/browser";
     selector: 'app',
     template: `
 
+            <div>Pick a Color:</div>
+
+            <div class="color-picker">
+                <div class="color-sample color-sample-red"></div>
+                <div class="color-sample color-sample-blue"></div>
+            </div>
+
+            <div>Preview Text Color</div>
+
+            <button>Reset</button>
 
         `
 })

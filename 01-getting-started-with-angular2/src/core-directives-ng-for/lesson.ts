a2bc56c454d5f43bed192187d7063a96fe75c384
angular-academy-devs <contact@angular-academy.io>
2016-05-04 09:10:56 +0200

    ngFor Lesson

---
+++ 01-getting-started-with-angular2/src/core-directives-ng-for/lesson.ts
@@ -0,0 +1,32 @@
+
+import {Component} from "angular2/core";
+import {bootstrap} from "angular2/platform/browser";
+import {ColorPicker} from "./color_picker";
+import {ColorPreviewer} from "./color_previewer";
+import {RED} from "./constants";
+
+
+@Component({ 
+    selector: 'app',
+    directives: [ColorPreviewer, ColorPicker],
+    template: `
+            
+            <color-picker #picker [color]="color" (color)="color = $event">  
+            </color-picker>
+                
+            <color-previewer #previewer [color]="color"></color-previewer>
+                
+            <button (click)="picker.reset()">Reset</button>
+
+        `
+})
+export class App {
+
+    color = RED;
+
+}
+
+
+
+bootstrap(App);
+

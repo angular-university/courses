c94c5f8d7f4e980405ad74bbbb332d40ec48d837
Your Name <you@example.com>
2016-05-05 21:12:16 +0200

    ngClass ngStyle Lesson

---
+++ 01-getting-started-with-angular2/src/core-directives-ngif/lesson.ts
@@ -0,0 +1,20 @@
+
+import {Component} from "@angular/core";
+import {bootstrap} from "@angular/platform-browser-dynamic";
+
+
+@Component({ 
+    selector: 'app',
+    template: `
+
+
+
+        `
+})
+export class App {
+
+
+}
+
+bootstrap(App);
+

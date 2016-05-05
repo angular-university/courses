2f630a9763e131a59c9e88d1bfb1d9bccd875e3f
Your Name <you@example.com>
2016-05-05 21:55:58 +0200

    ngClass ngStyle Lesson

--- 01-getting-started-with-angular2/src/core-directives-ngif/lesson.ts
+++ 01-getting-started-with-angular2/src/core-directives-ngif/lesson.ts
@@ -7,12 +7,34 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
     selector: 'app',
     template: `
 
+            <div class="toggle-panel" *ngIf="show" 
+                [hidden]="hidden" [style.visibility]="visibility">Toggle Me</div>
 
+            
+            <button (click)="toggleShow()">Show</button>
+            <button (click)="toggleHidden()">Hidden</button>
+            <button (click)="toggleVisible()">Visible</button>
+            
 
         `
 })
 export class App {
 
+    show = true;
+    hidden = false;
+    visibility = 'visible';
+
+    toggleShow() {
+        this.show = !this.show;
+    }
+
+    toggleHidden() {
+        this.hidden = !this.hidden;
+    }
+
+    toggleVisible() {
+        this.visibility = this.visibility == 'visible' ? 'hidden' : 'visible';
+    }
 
 }
 

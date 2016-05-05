ae63ddabb4f9eb59d811d292236eae7d815d0923
Your Name <you@example.com>
2016-05-05 22:39:12 +0200

    ngClass ngStyle Lesson

--- 01-getting-started-with-angular2/src/directives/lesson.ts
+++ 01-getting-started-with-angular2/src/directives/lesson.ts
@@ -7,7 +7,27 @@ import {bootstrap} from "@angular/platform-browser-dynamic";
     selector: 'app',
     template: `
 
+            <div show-one-container>
             
+                <ul class="tab-buttons">
+                    <li show-one-trigger="superman">Superman</li>
+                    <li show-one-trigger="batman">Batman</li>
+                    <li show-one-trigger="flash">Flash</li>
+                </ul>
+            
+                <div class="tab-panel" show-one-item="superman">
+                    <div class="superman"></div>
+                </div>
+                
+                <div class="tab-panel" show-one-item="batman">
+                    <div class="batman"></div>
+                </div>
+                
+                <div class="tab-panel" show-one-item="flash">
+                    <div class="flash"></div>
+                </div>
+            
+            </div>
 
         `
 })

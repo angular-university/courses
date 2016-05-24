ef93b7f67dd1d77c04f3ac26abc4c08c712d5fac
Your Name <you@example.com>
2016-05-24 13:33:34 +0200

    directives lesson

--- 01-getting-started-with-angular2/src/components-inputs-and-outputs/main.ts
+++ 01-getting-started-with-angular2/src/components-inputs-and-outputs/main.ts
@@ -1,10 +1,12 @@
 
 import {Component} from "@angular/core";
 import {bootstrap} from "@angular/platform-browser-dynamic";
+import {SearchBox} from "./search-box.component";
 
 @Component({
     selector:'main',
-    template: ``
+    directives: [SearchBox],
+    template: ` <search-box></search-box>`
 })
 export class Main {
 
---
+++ 01-getting-started-with-angular2/src/components-inputs-and-outputs/search-box.component.ts
@@ -0,0 +1,13 @@
+
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'search-box',
+    template: `<input placeholder="Search"><button>Clear</button>`
+})
+export class SearchBox {
+
+
+
+
+}

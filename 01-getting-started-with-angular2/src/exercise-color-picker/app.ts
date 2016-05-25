fa26011dae7c58ecd4bfee0f44885387ee1e550f
Your Name <you@example.com>
2016-05-25 15:14:02 +0200

    directives lesson

---
+++ 01-getting-started-with-angular2/src/exercise-color-picker/app.ts
@@ -0,0 +1,15 @@
+
+import {Component} from "@angular/core";
+import {bootstrap} from "@angular/platform-browser-dynamic";
+
+
+
+@Component({
+    selector:'app',
+    template: `<h1>Hello !</h1>`
+})
+export class App {
+
+}
+
+bootstrap(App);

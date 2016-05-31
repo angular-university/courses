3dabb3cacf0e68ac825a971de1b2f1200998dfdd
angular-academy-devs <contact@angular-academy.io>
2016-05-31 15:25:46 +0200

    course logos

--- 01-getting-started-with-angular2/lessons-server.ts
+++ 01-getting-started-with-angular2/lessons-server.ts
@@ -32,7 +32,7 @@ app.route('/lessons')
 
     });
 
-var server = app.listen(8081, function() {
+var server = app.listen(8080, function() {
     console.log("Server running at http://localhost:" + server.address().port);
 });
 

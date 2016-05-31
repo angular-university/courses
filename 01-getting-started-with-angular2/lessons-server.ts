5c5dbcd76026552a8b8f1f47713c72fbfdc2252d
angular-academy-devs <contact@angular-academy.io>
2016-05-31 15:10:42 +0200

    course logos

--- 01-getting-started-with-angular2/lessons-server.ts
+++ 01-getting-started-with-angular2/lessons-server.ts
@@ -4,12 +4,9 @@ declare const require;
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('lodash');
-var httpProxy = require('http-proxy');
 
 
 
-
-var proxy = httpProxy.createProxyServer();
 
 var app = express();
 
@@ -18,6 +15,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.text());
 
 
+app.route('/lessons')
+    .get((req, res) => {
+
+    })
+    .put((req, res) => {
+        var json = req.body;
+
+    })
+    .delete((req,res) => {
+        console.log('removing todo with id = ' + req.query.id);
+
+    })
+    .post((req, res) => {
+        todos.push(req.body);
+
+    });
+
 var server = app.listen(8081, function() {
     console.log("Server running at http://localhost:" + server.address().port);
 });
+

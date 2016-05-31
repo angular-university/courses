6ae41a9ec54195dec63dc97a5230554983f28bf9
angular-academy-devs <contact@angular-academy.io>
2016-05-31 16:41:09 +0200

    course logos

--- 01-getting-started-with-angular2/src/services-intro/lessons-list.component.ts
+++ 01-getting-started-with-angular2/src/services-intro/lessons-list.component.ts
@@ -3,10 +3,13 @@ import {Component, Input} from "@angular/core";
 
 
 @Component({
-    selector:'lessons-list',   
+    selector:'lessons-list',
     template: `
             <table class="lessons-list card card-strong">
                 <tr *ngFor="let lesson of lessons">
+                    <td>
+                        <img class="lesson-logo" src="https://material.angularjs.org/latest/img/icons/angular-logo.svg">  
+                    </td>
                     <td>
                         {{lesson.description}}    
                     </td>

44fba150b7e425819a05a2d8322e4451a0108160
angular-academy-devs <contact@angular-academy.io>
2016-05-31 14:42:27 +0200

    course logos

---
+++ 01-getting-started-with-angular2/src/lessons-server/server.ts
@@ -0,0 +1,11 @@
+declare const require;
+
+
+var express = require('express');
+
+//var bodyParser = require('body-parser');
+//var _ = require('lodash');
+//var httpProxy = require('http-proxy');
+
+
+console.log("Hello World !");

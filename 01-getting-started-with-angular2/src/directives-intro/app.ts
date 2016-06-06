cddcbf8bcf2f5907c85228a6723b68476b31445e
angular-academy-devs <contact@angular-academy.io>
2016-06-06 11:49:43 +0200

    directives intro section

--- 01-getting-started-with-angular2/src/directives-intro/app.ts
+++ 01-getting-started-with-angular2/src/directives-intro/app.ts
@@ -11,7 +11,8 @@ import {CollapseOnClick} from "./collapse-on-click.directive";
     template: `
         
         <div collapse-on-click [collapsed]="!open" (collapsed)="open = !$event" class="card card-strong disable-text-selection">
-            <i class="md-icon collapsible-indicator collapsible-indicator-down">arrow_drop_down</i> 
+            <i class="md-icon collapsible-indicator" *ngIf="open">arrow_drop_down</i>
+             <i class="md-icon collapsible-indicator" *ngIf="!open">arrow_drop_up</i>
             <div class="collapsible-section">  
                 This page section is collapsible, double click it and it will collapse or expand.
             </div>

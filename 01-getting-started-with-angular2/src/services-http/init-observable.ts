f7d2c694c9027ea6982eb41b135c81d24d3feb4c
Your Name <you@example.com>
2016-06-13 12:52:14 +0200

    services in detail

--- 01-getting-started-with-angular2/src/services-http/init-observable.ts
+++ 01-getting-started-with-angular2/src/services-http/init-observable.ts
@@ -1,13 +1,21 @@
 
-
 import {Observable} from "rxjs/Observable";
+declare function fetch(url:string);
+
+
+
 
 export function initObservable() {
 
-
     var keys$ = Observable.fromEvent(document, 'keyup')
-        .do((keyUp:KeyboardEvent) => console.log(keyUp.key));
+                    .do((keyUp:KeyboardEvent) => console.log(keyUp.key));
 
     keys$.subscribe();
 
+
+    fetch('/lessons')
+        .then(res => res.json())
+        .then(lessons => console.log(lessons));
+
 }
+

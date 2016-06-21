c2626933804598499468ef096b121ad26828b064
angular-academy-devs <contact@angular-academy.io>
2016-06-21 14:53:45 +0200

    template driven forms

--- 01-getting-started-with-angular2/src/template-driven-forms/app.ts
+++ 01-getting-started-with-angular2/src/template-driven-forms/app.ts
@@ -12,11 +12,11 @@ import {Lesson} from './lesson';
 
             <h3>Create Lesson Form - Template Driven</h3>
             
-            <form #f="ngForm" (ngSubmit)="createLesson()" autocomplete="off" novalidate>
+            <form #f="ngForm" (ngSubmit)="createLesson(title)" autocomplete="off" novalidate>
                 <p>
                     <label>Title:</label>
-                    <input  [(ngModel)]="lesson.title" name="title" required minlength="30" maxlength="50">
-                </p>
+                    <input  [(ngModel)]="lesson.title" name="title" required minlength="30" maxlength="50" #title="ngForm">
+                    <span *ngIf="title?.errors?.required">Title is mandatory</span>
                 <p>
                     <label>Duration (in sec.):</label>
                     <input [(ngModel)]="lesson.duration" name="duration" required pattern="[0-9]+">
@@ -26,7 +26,7 @@ import {Lesson} from './lesson';
                     <textarea [(ngModel)]="lesson.description" name="description" required minlength="150"></textarea>
                 </p>
                 <p>
-                    <button type="submit" [disabled]="!f.valid">Create Lesson</button>
+                    <button type="submit" [disabled]="false">Create Lesson</button>
                 </p>
             </form>                        
 
@@ -36,6 +36,11 @@ export class App {
     
 
     lesson = new Lesson("Title goes here",0,  "Description goes here");
+    
+    
+    createLesson(title) {
+        debugger;
+    }
 
 }
 

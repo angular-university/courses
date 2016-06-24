e066b1e020d5d35ca253cba6db37f236dfc442c8
angular-academy-devs <contact@angular-academy.io>
2016-06-24 11:07:43 +0200

    initial version - hello world

--- 01-getting-started-with-angular2/system.config.js
+++ 01-getting-started-with-angular2/system.config.js
@@ -1,33 +1,71 @@
-System.config({
-    map: {
-        'rxjs': 'node_modules/rxjs',
-        '@angular': 'node_modules/@angular',
-        'app': 'dist'
-    },
-    packages: {
-        '@angular/core': {
-            main: 'index'
+(function (global) {
+
+    //map tells the System loader where to look for things
+    var map = {
+
+        '@angular': 'node_modules/@angular', // sufficient if we didn't pin the version
+        '@angular/router': 'node_modules/@angular/router' ,
+        '@angular/forms': 'node_modules/@angular/forms',
+        'angular2-in-memory-web-api': 'https://npmcdn.com/angular2-in-memory-web-api', // get latest
+        'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6',
+        'ts': 'https://npmcdn.com/plugin-typescript@4.0.10/lib/plugin.js',
+        'typescript': 'https://npmcdn.com/typescript@1.9.0-dev.20160409/lib/typescript.js',
+    };
+
+    //packages tells the System loader how to load when no filename and/or no extension
+    var packages = {
+        'rxjs': {defaultExtension: 'js'},
+        'angular2-in-memory-web-api': {main: 'index.js', defaultExtension: 'js'},
+    };
+
+    var ngPackageNames = [
+        'common',
+        'compiler',
+        'core',
+        'http',
+        'platform-browser',
+        'platform-browser-dynamic',
+        'router-deprecated',
+        'upgrade',
+    ];
+
+    // Add map entries for each angular package
+    ngPackageNames.forEach(function (pkgName) {
+        map['@angular/' + pkgName] = 'node_modules/@angular/' + pkgName;
+    });
+
+    // Add package entries for angular packages
+    ngPackageNames.forEach(function (pkgName) {
+
+        // Bundled (~40 requests):
+        packages['@angular/' + pkgName] = {main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js'};
+
+        // Individual files (~300 requests):
+        //packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    });
+
+    // No umd for router yet
+    packages['@angular/router'] = {main: 'index.js', defaultExtension: 'js'};
+
+    // Forms not on rc yet
+    packages['@angular/forms'] = {main: 'index.js', defaultExtension: 'js'};
+
+    var config = {
+        // DEMO ONLY! REAL CODE SHOULD NOT TRANSPILE IN THE BROWSER
+        transpiler: 'ts',
+        typescriptOptions: {
+            tsconfig: true
         },
-        '@angular/compiler': {
-            main: 'index'
+        meta: {
+            'typescript': {
+                "exports": "ts"
+            }
         },
-        '@angular/common': {
-            main: 'index'
-        },
-        '@angular/platform-browser': {
-            main: 'index'
-        },
-        '@angular/platform-browser-dynamic': {
-            main: 'index'
-        },
-        '@angular/forms': {
-            main: 'index'
-        },
-        'rxjs': {
-            main: "Rx"
-        },
-        'app': {
-            main: '01-mvc-hello-world/hello_world'
-        }
-    }
-});
+        map: map,
+        packages: packages
+    };
+
+    document.SYSTEMJS_CONFIG = config;
+
+})(this);
+

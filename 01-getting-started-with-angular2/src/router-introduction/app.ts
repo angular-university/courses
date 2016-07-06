92b31812d601a757182f7e41157e6b239e8fbf47
angular-academy-devs <contact@angular-academy.io>
2016-07-06 19:43:58 +0200

    prepare Router lessons

--- 01-getting-started-with-angular2/src/router-introduction/app.ts
+++ 01-getting-started-with-angular2/src/router-introduction/app.ts
@@ -1,14 +1,16 @@
 
 import {Component} from "@angular/core";
 import {bootstrap} from "@angular/platform-browser-dynamic";
-
+import { ROUTER_DIRECTIVES , provideRouter} from '@angular/router';
+import {routes} from "./router-config";
 
 
 @Component({
     selector:'app',
+    directives: [ROUTER_DIRECTIVES],
     template: `
 
-            <h1>Hello World !</h1>
+            <h1>Hello Router World !</h1>
             
         `
 })
@@ -22,5 +24,5 @@ export class App {
 
 
 bootstrap(App, [
-
+    provideRouter(routes)
 ]);
---
+++ 01-getting-started-with-angular2/src/router-introduction/router-config.ts
@@ -0,0 +1,8 @@
+
+import {RouterConfig } from '@angular/router';
+
+
+export const routes: RouterConfig = [
+    { path: 'crisis-center', component: null },
+    { path: 'heroes', component: null }
+];

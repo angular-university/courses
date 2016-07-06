3f12ea0f35b734a7680bf356663599a5d96a0352
angular-academy-devs <contact@angular-academy.io>
2016-07-06 10:53:46 +0200

    prepare Forms lessons

--- 01-getting-started-with-angular2/src/template-driven-forms/app.ts
+++ 01-getting-started-with-angular2/src/template-driven-forms/app.ts
@@ -13,24 +13,44 @@ import {Lesson, StudentLevel} from './lesson';
             <h3>Create Lesson Form - Template Driven</h3>
             
             <form #f="ngForm" (ngSubmit)="createLesson(title)" autocomplete="off" novalidate>
-                <p>
-                    <label>Title:</label>
-                    <input  [(ngModel)]="lesson.title" name="title" required minlength="5" maxlength="20" #title="ngModel">
-                    <div class="field-error-message" *ngIf="title?.errors?.required">field is mandatory</div>
-                    <div class="field-error-message" *ngIf="title?.errors?.minlength">min 5 chars</div>
-                <p>
-                    <label>Duration:</label>
-                    <input [(ngModel)]="lesson.duration" name="duration" required pattern="[0-9]+">
-                </p>
-                <p>
-                    <label>Description:</label>
-                    <textarea [(ngModel)]="lesson.description" name="description" required minlength="150"></textarea>
-                </p>
-                <p>
-                    <label>Level:</label>
-                     <input type="radio" name="level" [(ngModel)]="lesson.level" value="BEGINNER">Beginner
-                     <input type="radio" name="level" [(ngModel)]="lesson.level" value="ADVANCED">Advanced
-                </p>
+            
+                <fieldset ngModelGroup="summary" #summary="ngModelGroup">
+                    <legend [ngStyle]="{color: !summary.valid && !summary.pristine ? 'red' : 'black'}">Summary</legend>
+                   <p>
+                        <label>Title:</label>
+                        <input  [(ngModel)]="lesson.title" name="title" required minlength="5" maxlength="20" #title="ngModel">
+                        <div class="field-error-message" *ngIf="title?.errors?.required">field is mandatory</div>
+                        <div class="field-error-message" *ngIf="title?.errors?.minlength">min 5 chars</div>
+                    <p>
+                        <label>Duration:</label>
+                        <input [(ngModel)]="lesson.duration" name="duration" required pattern="[0-9]+">
+                    </p>
+                    <p>
+                        <label>Level:</label>
+                         <input type="radio" name="level" [(ngModel)]="lesson.level" value="BEGINNER">Beginner
+                         <input type="radio" name="level" [(ngModel)]="lesson.level" value="ADVANCED">Advanced
+                    </p>
+                         
+                </fieldset>
+                
+                <fieldset ngModelGroup="text">
+                    <legend>Long Text Fields</legend>
+                    <p>
+                        <label>Description:</label>
+                        <textarea [(ngModel)]="lesson.description" name="description" required minlength="150"></textarea>
+                    </p>
+                    <p>
+                        <label>Transcript:</label>
+                        <textarea [(ngModel)]="lesson.description" name="description" required minlength="150"></textarea>
+                    </p>                    
+                    <p>
+                        <label>Level:</label>
+                         <input type="radio" name="level" [(ngModel)]="lesson.level" value="BEGINNER">Beginner
+                         <input type="radio" name="level" [(ngModel)]="lesson.level" value="ADVANCED">Advanced
+                    </p>
+                         
+                </fieldset>                
+            
                 <p>
                     <button type="submit" [disabled]="false">Create Lesson</button>
                 </p>

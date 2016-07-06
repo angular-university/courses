766e092e58f9d73c8496bc5033581d65a85635ab
angular-academy-devs <contact@angular-academy.io>
2016-07-06 17:28:07 +0200

    prepare Router lessons

---
+++ 01-getting-started-with-angular2/src/router-introduction/app.ts
@@ -0,0 +1,26 @@
+
+import {Component} from "@angular/core";
+import {bootstrap} from "@angular/platform-browser-dynamic";
+
+
+
+@Component({
+    selector:'app',
+    template: `
+
+            <h1>Hello World !</h1>
+            
+        `
+})
+export class App {
+
+
+
+}
+
+
+
+
+bootstrap(App, [
+
+]);

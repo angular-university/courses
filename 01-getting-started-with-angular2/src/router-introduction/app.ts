4f376d5311f73f54831f2ff525ed8f67fe2b6006
Your Name <you@example.com>
2016-07-16 10:58:19 +0200

    router lessons

--- 01-getting-started-with-angular2/src/router-introduction/app.ts
+++ 01-getting-started-with-angular2/src/router-introduction/app.ts
@@ -1,7 +1,7 @@
 
 import {Component} from "@angular/core";
 import {bootstrap} from "@angular/platform-browser-dynamic";
-import { ROUTER_DIRECTIVES , provideRouter} from '@angular/router';
+import {ROUTER_DIRECTIVES, provideRouter} from "@angular/router";
 import {routes} from "./router-config";
 import {LessonsService} from "./lessons.service";
 import {HTTP_PROVIDERS} from "@angular/http";
@@ -13,13 +13,14 @@ import {HTTP_PROVIDERS} from "@angular/http";
     template: `
 
     <header class="l-header v-center-parent">
-        <img class="v-center" src="https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png">
+        <img class="v-center" 
+            src="https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png">
         <ul class="top-menu disable-link-styles">
             <li><a [routerLink]="['lessons']">Lessons</a></li>         
         </ul>        
     </header>
 
-    <main class="l-main">
+    <main class="l-main l-sample-app">
 
         <img class="youtube-logo card card-strong" src="https://i.ytimg.com/vi/s5y-4EpmfRQ/maxresdefault.jpg">
         <div class="lesson lesson-forms">

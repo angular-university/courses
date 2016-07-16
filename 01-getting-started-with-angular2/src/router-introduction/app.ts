cd6432c4516bd512e0f79db6685f055dd47caff8
Your Name <you@example.com>
2016-07-16 11:36:37 +0200

    router lessons

--- 01-getting-started-with-angular2/src/router-introduction/app.ts
+++ 01-getting-started-with-angular2/src/router-introduction/app.ts
@@ -16,8 +16,8 @@ import {HTTP_PROVIDERS} from "@angular/http";
         <img class="v-center" 
             src="https://angular-academy.s3.amazonaws.com/main-logo/main-page-logo-small-hat.png">
         <ul class="top-menu disable-link-styles">
-            <li><a [routerLink]="['home']">Home</a></li>
-            <li><a [routerLink]="['courses']">Courses</a></li>
+            <li><a routerLink href="home">Home</a></li>
+            <li><div routerLink target="courses">Courses</div></li>
             <li><a [routerLink]="['lessons']">All Lessons</a></li>
         </ul>        
     </header>

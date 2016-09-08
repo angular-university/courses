72ea7542289cd5a43b8f88e832f5837cef5a95ff
Your Name <you@example.com>
2016-09-08 10:55:02 +0200

    final project - build an app with Angular 2 and Firebase

---
+++ 01-getting-started-with-angular2/final-project/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import {FinalProjectPage} from "./app.po";
+
+describe('final-project App', function() {
+  let page: FinalProjectPage;
+
+  beforeEach(() => {
+    page = new FinalProjectPage();
+  });
+
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
+  });
+});

76d9205bc2c314f410f4b881a2bc8ba4a6ac4218
Your Name <you@example.com>
2016-09-08 12:11:19 +0200

    final project - build an app with Angular 2 and Firebase

--- 01-getting-started-with-angular2/final-project/src/app/app.component.ts
+++ 01-getting-started-with-angular2/final-project/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component} from "@angular/core";
 
+declare const firebase;
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,14 @@ import {Component} from "@angular/core";
 })
 export class AppComponent {
   title = 'Final Project ongoing !';
+
+
+  constructor() {
+
+    firebase.database().ref().on('value', (snap) => {
+      console.log(snap.val());
+    });
+
+  }
+
 }

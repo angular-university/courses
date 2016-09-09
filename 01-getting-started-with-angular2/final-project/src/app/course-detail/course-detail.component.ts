e9672bc9bf7da53919fc9a8ffd575a58dd5c1057
angular-academy-devs <contact@angular-academy.io>
2016-09-09 13:22:16 +0200

    signup and get angular 2 for beginners course

---
+++ 01-getting-started-with-angular2/final-project/src/app/course-detail/course-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-detail',
+  templateUrl: './course-detail.component.html',
+  styleUrls: ['./course-detail.component.css']
+})
+export class CourseDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

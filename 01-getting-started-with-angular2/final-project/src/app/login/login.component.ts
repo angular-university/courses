d9b866561c192ffde2cb8241900daf82e59838fd
angular-academy-devs <contact@angular-academy.io>
2016-09-18 16:16:38 +0200

    Build an App with Firebase

--- 01-getting-started-with-angular2/final-project/src/app/login/login.component.ts
+++ 01-getting-started-with-angular2/final-project/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['',Validators.required],
+      password: ['',Validators.required]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  login() {
+
   }
 
 }

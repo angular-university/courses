2ed332f5dff864b9f98e0b44d69fd8c7d201d342
angular-academy-devs <contact@angular-academy.io>
2016-09-18 17:47:08 +0200

    Build an App with Firebase

--- 01-getting-started-with-angular2/final-project/src/app/register/register.component.ts
+++ 01-getting-started-with-angular2/final-project/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import {AuthService} from "../auth.service";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+
+
+  }
 
   ngOnInit() {
+    this.form = this.fb.group({
+      email: ['',Validators.required],
+      password: ['',Validators.required],
+      confirm: ['',Validators.required]
+    });
+  }
+
+
+  signUp() {
+
+  }
+
+  isPasswordMatch() {
+    const val = this.form.value;
+    return val && val.password && val.password == val.confirm;
   }
 
 }

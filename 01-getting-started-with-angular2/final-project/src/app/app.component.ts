67dffd8e2a2abd80c59790641f24f0cb208cf7fd
Your Name <you@example.com>
2016-09-26 12:13:03 +0200

    Build an app with Angular 2 and Firebase

--- 01-getting-started-with-angular2/final-project/src/app/app.component.ts
+++ 01-getting-started-with-angular2/final-project/src/app/app.component.ts
@@ -14,30 +14,48 @@ export class AppComponent {
     courses$:FirebaseListObservable<any>;
     lesson$:FirebaseObjectObservable<any>;
 
-    firstCourse: any;
+    firstCourse:any;
 
 
     constructor(private af:AngularFire) {
 
         this.courses$ = af.database.list('courses');
+
         this.courses$.subscribe(console.log);
 
-        this.lesson$ = af.database.object('lessons/-KSH8w9OnJMNzIGUW98c');
+        this.lesson$ = af.database.object('lessons/-KS_qPw2zNFR1Ju9Cb2H');
+
         this.lesson$.subscribe(console.log);
+
+        this.courses$.map(courses => courses[0])
+            .subscribe(
+                course => this.firstCourse = course
+            );
 
     }
 
 
     listPush() {
 
+        this.courses$.push({description: 'TEST NEW COURSE'})
+            .then(
+                () => console.log('List Push Done'),
+                console.error
+            );
+
     }
 
 
     listRemove() {
+
+        this.courses$.remove(this.firstCourse);
+
     }
 
 
     listUpdate() {
+
+        this.courses$.update(this.firstCourse, {description: 'Angular 2 HTTP Modified'});
 
     }
 
@@ -45,10 +63,18 @@ export class AppComponent {
 
     objUpdate() {
 
+        this.lesson$.update({description: 'NEW DESCRIPTION'});
+
     }
 
     objSet() {
 
+        this.lesson$.set({description: 'NEW DESCRIPTION'});
+
+    }
+
+    objRemove() {
+        this.lesson$.remove();
     }
 
 

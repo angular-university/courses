7b7cb88b8fed7cb4c1822a8282f1452a7f5295c3
Your Name <you@example.com>
2016-09-26 11:47:54 +0200

    Build an app with Angular 2 and Firebase

--- 01-getting-started-with-angular2/final-project/src/app/app.component.ts
+++ 01-getting-started-with-angular2/final-project/src/app/app.component.ts
@@ -25,45 +25,29 @@ export class AppComponent {
         this.lesson$ = af.database.object('lessons/-KSH8w9OnJMNzIGUW98c');
         this.lesson$.subscribe(console.log);
 
-        this.courses$
-            .map(courses => courses[0])
-            .subscribe(course => this.firstCourse = course);
     }
 
 
     listPush() {
-        this.courses$.push({description: 'TEST'})
-            .then(
-                () => console.log('List Push Done'),
-                console.error
-            );
+
     }
 
 
     listRemove() {
-        this.courses$.remove(this.firstCourse)
-            .then(
-                () => console.log('List Remove Done'),
-                console.error
-            );
     }
 
 
     listUpdate() {
-        this.courses$.update(this.firstCourse, {description: 'Test'});
+
     }
 
 
 
     objUpdate() {
 
-        this.lesson$.update({description:'TEST'});
-
     }
 
     objSet() {
-
-        this.lesson$.set({description:'TEST'});
 
     }
 
